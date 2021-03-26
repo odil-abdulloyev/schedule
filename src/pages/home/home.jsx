2be34e01f38b5f1d718a94986ae5edfaa6880bc5
Odil Abdulloyev <abdulloyevodil@mail.ru>
2021-03-26 17:41:11 +0500

    feat: add login link

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,10 +1,17 @@
 import { Container } from '@material-ui/core';
 import React from 'react';
 import Schedule from '../../components/schedule/schedule';
+import { Link } from 'react-router-dom';
+import { Typography, Link as MaterialLink } from '@material-ui/core';
 
 export default function Home() {
   return (
     <Container maxWidth='md'>
+      <Link to='/login'>
+        <Typography>
+          <MaterialLink>Login</MaterialLink>
+        </Typography>
+      </Link>
       <Schedule group='321-19' />
     </Container>
   );
